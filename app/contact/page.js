d3c7700cb690f0f36e2804cfda748196fcf5adf8
stevieg51 <gordonsteven451@gmail.com>
2023-12-20 14:55:21 -0500

    Just getting stated and getting used to the project for now.
    Getting the contact page started, really just committing the little bit I've done so far so I can access it from my desktop once I get home from travelling this week. I am confident in having it finished by Saturday.

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import NavBar from '../../components/NavBar';
+import ContactPageForm from './page2';
 
 function ContactPage()
 {
   return (
     <>
       <NavBar />
-      <div className='flex flex-col items-center justify-center py-24 bg-[#fff]'>
+      <div className='flex flex-col items-center justify-center py-10 bg-[#fff]'>
       
 
       {/* Main Text */}
       <div className='flex flex-col text-center mx-auto max-w-[1260px] items-center justify-center pt-40'>
-        <h1 className='text-5xl sm:text-8x1 font-bold text-black tracking-tight'>Contact {""}
-          <span className='bg-gradient-to-r from-[#FFFF00] to-[#ffaf7a] text-transparent bg-clip-text'>
-            page
-          </span>
+        <h1 className=' ui-sans-serif text-7xl sm:text-8x1 text-black tracking-tight mt-0 mb-20'>Contact {""}
         </h1>
+      <div>
+         <p className='my-20 text-4xl sm:text-8x1 text-black tracking-tight'> Get in touch </p>
+          </div>
+      <div>
+      <blockquote class=" text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9 w-96">
+        <p className=' my-10  sm:text-8x1 text-black tracking-tight'> We would love to hear any questions you may have. Please drop us a line. </p>
+        </blockquote>
       </div>
-
+      <div> the arrow </div>
+    </div>
+    <ContactPageForm/>
     </div>
     </>
   )
---
+++ app/contact/page2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ContactPageForm() {
+
+    return (
+        <>
+        <p> Place for form</p>
+        
+        </>
+    )
+}
