79afe58d32d167db08b58555fd474c20e23d2764
Knighton910 <keltheceo@gmail.com>
2025-03-05 17:41:30 -0600

    updated subscription cards

--- app/subscription/page.js
+++ app/subscription/page.js
@@ -1,26 +1,13 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import NavBar from "../../components/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "../../components/Footer";
-import StandardTier from "../../components/StandardTier";
-import BasicTier from "../../components/BasicTier";
-import PremiumTier from "../../components/PremiumTier";
-import SubscriptionCard from "./SubCard";
-import { features, services, cares, subscriptionPlans } from "./data";
-// import SwipeableViews from 'react-swipeable-views';
-// import { autoPlay } from 'react-swipeable-views-utils';
 import {
   Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
   Button,
 } from "@material-tailwind/react";
-
-// const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const CustomLink = ({ href, children }) => {
   return (
@@ -30,184 +17,261 @@ const CustomLink = ({ href, children }) => {
   );
 };
 
-function CheckIcon() {
+function BasicBadge({ price }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="white"
-      className="h-3 w-3"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M4.5 12.75l6 6 9-13.5"
-      />
-    </svg>
+    <div className="relative flex items-center bg-gradient-to-r from-yellow-400 to-yellow-600 rounded-full px-4 py-2 w-[220px]">
+      <h2 className="text-xl text-white z-10">BASIC</h2>
+      <span className="text-lg bg-white text-black px-4 py-1 rounded-full absolute right-2 z-20">
+        {price}
+      </span>
+      <div className="absolute inset-0 bg-gradient-to-r from-yellow-500 to-gold opacity-20 rounded-full z-0"></div>
+    </div>
   );
 }
 
+function StandardBadge({ price }) {
+  return (
+    <div className="relative flex items-center bg-purple-700 rounded-full px-4 py-2 w-[220px]">
+      <h2 className="text-xl text-white z-10">STANDARD</h2>
+      <span className="text-lg bg-white text-black px-4 py-1 rounded-full absolute right-2 z-20">
+        {price}
+      </span>
+      <div className="absolute inset-0 bg-purple-800 opacity-20 rounded-full z-0"></div>
+    </div>
+  );
+}
+
+function PremiumBadge({ price }) {
+  return (
+    <div className="relative flex items-center bg-red-600 rounded-full px-4 py-2 w-[220px]">
+      <h2 className="text-xl text-white z-10">PREMIUM</h2>
+      <span className="text-lg bg-white text-black px-4 py-1 rounded-full absolute right-2 z-20">
+        {price}
+      </span>
+      <div className="absolute inset-0 bg-red-700 opacity-20 rounded-full z-0"></div>
+    </div>
+  );
+}
+
+function VipBadge({ price }) {
+  return (
+    <div className="relative flex items-center bg-gradient-to-r from-yellow-400 to-pink-500 rounded-full px-4 py-2 w-[220px]">
+      <h2 className="text-xl text-white z-10">VIP</h2>
+      <span className="text-lg bg-white text-black px-4 py-1 rounded-full absolute right-2 z-20">
+        {price}
+      </span>
+      <div className="absolute inset-0 bg-gradient-to-r from-yellow-500 to-pink-600 opacity-20 rounded-full z-0"></div>
+    </div>
+  );
+}
+
+
+
+
 function Subscription() {
-  const [isMonthly, setIsMonthly] = useState(true);
-
-  const toggleSubscription = () => {
-    setIsMonthly(!isMonthly);
-  };
-
   return (
     <>
       <NavBar />
-      <div className="min-h-screen bg-white">
-        <h1 className="text-4xl px-10 py-20 mt-10 mb-10">
-          Explore our housekeeping subscriptions.
-        </h1>
-
-        <div className="flex overflow-x-auto space-x-6 mx-5 py-10 px-4 sm:px-0 scrollbar-hide justify-center">
-          {subscriptionPlans.map((plan, index) => (
-            <SubscriptionCard
-              key={index}
-              title={plan.title}
-              description={plan.description}
-              imageSrc={plan.imageSrc}
-              linkHref={plan.linkHref}
-              linkText={plan.linkText}
-              features={plan.features}
-              price={plan.price}
+      <main className="min-h-screen bg-gray-100 text-gray-900">
+        {/* Page Container */}
+        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          
+          {/* Hero Section */}
+          <section className="text-center space-y-4">
+            <Image
+              src="/vip/TomBig.png"
+              height={100}
+              width={1000}
+              alt="mobile VIP"
+              className="z-1 block rounded-3xl md:hidden mt-20"
             />
-          ))}
-        </div>
+            <div className="flex justify-left">
+              <h2 className="bg-yellow-200 rounded-2xl px-4 py-1 max-w-md">
+                Now accepting applications
+              </h2>
+            </div>
+          </section>
 
-        {/* <Image
-        src="/2.png"
-        width={1500}
-        height={100}
-        alt='desktop view VIP'
-        className='z-1 hidden md:block'
-      />
+          {/* Waitlist Section */}
+<section>
+  <div>
+    <h1 className="py-5 text-6xl font-serif">WAITLIST</h1>
 
+    <p className="px-4">
+      We're excited to welcome our early adopters in San Francisco! There are only{" "}
+      <span className="italic">(60 spots available)</span>. This isn’t about artificial scarcity—our availability is limited because we have a dedicated team of full-time housekeepers.
+    </p>
+
+    <br />
+
+    <p className="px-4">
+      To secure your spot, a reservation fee of <span className="italic">1/3 of your chosen subscription</span> is required. Enjoy effortless housekeeping with a plan that suits you. Join our exclusive waitlist today and be among the first in San Francisco to experience seamless, subscription-based home cleaning.
+    </p>
+
+    <br />
+
+    <p className="px-4 font-semibold">Reserve your spot now!</p>
+
+    <Button className="bg-blue-600 text-xl text-white px-5 mt-5 rounded-3xl">
+      Join the waitlist
+    </Button>
+  </div>
+</section>
+
+<section className="my-20">
+  <div className="flex items-center justify-between px-4">
+    {/* Left side text */}
+    <p className="text-left text-4xl font-semibold leading-tight">
+      Explore our <br />
+      <span className="text-blue-600">monthly</span> <br />
+      housekeeping <br />
+      subscriptions
+    </p>
+
+    {/* Right side arrow */}
+    <Image
+      src="/down-arrow.png"
+      height={50}
+      width={50}
+      alt="directing flow"
+      className="w-20 h-20"
+    />
+  </div>
+</section>
+
+
+
+          {/* Responsive Subscription card Grid Section */}
+          <section className="mt-8 grid gap-8 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 auto-rows-fr">
+
+
+            {/* Responsive VIP Subscription Card */}
+            <section className="flex overflow-hidden flex-col items-center bg-white aspect-square">
+              <div className="flex relative flex-col px-3.5 pt-7 pb-20 w-full h-full aspect-[1.02]">
+                <Image
+                  src="/vip/Vip-card-image.png"
+                  height={100}
+                  width={1000}
+                  alt="Vip Subscription card"
+                  className="object-cover absolute inset-0 size-full"
+                />
+
+                <div className="absolute top-16 left-1/2 transform -translate-x-1/2 flex items-center space-x-4">
+                  <VipBadge price="$2,500" />
+                  <a href="/vipSub" className="bg-blue-600 text-white px-4 py-2 rounded-full text-sm font-semibold shadow-md hover:bg-blue-700 transition whitespace-nowrap inline-flex items-center">
+  View Details
+</a>
+                </div>
+
+                {/* Frosted Glass Effect */}
+                <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-20 backdrop-blur-md rounded-3xl px-6 py-4 whitespace-nowrap">
+                  <p className="text-white text-base text-center font-semibold">
+                    The Ultimate Housekeeping Experience
+                  </p>
+                </div>
+              </div>
+            </section>
+
+            {/* Responsive Premium Subscription Card */}
+  <section className="flex overflow-hidden flex-col items-center bg-white aspect-square w-full h-full">
+    <div className="flex relative flex-col pt-7 pb-20 w-full h-full">
       <Image
-        src="/vipnight.png"
+        src="/premium/Premium-card-image.png"
         height={100}
         width={1000}
-        alt='mobile VIP'
-        className='z-1 block md:hidden'
+        alt="Premium Subscription card"
+        className="object-cover absolute inset-0 w-full h-full"
       />
 
-       <Image
-        src="/3.png"
-        alt="desktop premium subscription"
-        width={1500}
-        height={100}
-        className='z-1 hidden md:block'
-      /> */}
-
-        {/* <Image
-        src="/premob.png"
-        height={100}
-        width={1000}
-        alt='mobile premium subscription'
-        className='z-1 block md:hidden'
-      />
-       <Image
-        src="/4.png"
-        alt="desktop Standard subscription"
-        width={1500}
-        height={100}
-        className='z-1 hidden md:block'
-      />
-
-      <Image
-        src="/stanmob.png"
-        height={100}
-        width={1000}
-        alt='mobile Standard subscription'
-        className='z-1 block md:hidden'
-      />
-       <Image
-        src="/1.png"
-        alt="Desktop basic subscription"
-        width={1500}
-        height={100}
-        className='z-1 hidden md:block'
-      />
-
-      <Image
-        src="/basmob.png"
-        height={100}
-        width={1000}
-        alt='mobile basic subscription'
-        className='z-1 block md:hidden'
-      /> */}
+      <div className="absolute top-16 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-full px-4">
+        <PremiumBadge price="$1,200" />
+        <a href="/premiumSub" className="bg-blue-600 text-white px-4 py-2 rounded-full text-sm font-semibold shadow-md hover:bg-blue-700 transition whitespace-nowrap inline-flex items-center">
+  View Details
+</a>
       </div>
 
-      <h1 className="text-5xl pt-20 pl-20 sm:text-8x1 font-bold dark:text-white text-black tracking-tight">
-        Additional {""}
-        <span className="bg-gradient-to-r from-[#5AC69F] to-[#3eb489] text-transparent bg-clip-text">
-          add-ons
-        </span>
-        <span className="bg-gradient-to-r from-[#0EC998] to-[#033024] text-transparent bg-clip-text">
-          +
-        </span>
-      </h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pt-20 mb-10 mx-10">
-        <div className="h-46 bg-teal-300 dark:bg-slate-800 p-6 rounded-lg drop-shadow-xl overflow-hidden">
-          <h2 className="text-2xl text-white dark:bg-slate-800 font-semibold p-5">
-            Fridge
-          </h2>
-          <p className="text-3xl text-white font-semibold mt-4">
-            {isMonthly ? "$50/month" : "$500/year"}
-          </p>
-          <Link href="/about">Learn more</Link>
-        </div>
-        <div className="h-46 bg-teal-300 dark:bg-slate-800 p-6 rounded-lg drop-shadow-xl overflow-hidden">
-          <h2 className="text-2xl text-white font-semibold dark:bg-slate-800 p-5">
-            Closet
-          </h2>
-          <p className="text-3xl text-white font-semibold mt-4">
-            {isMonthly ? "$60/month" : "$600/year"}
-          </p>
-          <Link href="/about">Learn more</Link>
-        </div>
-        <div className="h-46 bg-teal-300 dark:bg-slate-800 p-6 rounded-lg drop-shadow-xl overflow-hidden">
-          <h2 className="text-2xl text-white font-semibold p-5">Microwave</h2>
-          <p className="text-3xl text-white font-semibold mt-4">
-            {isMonthly ? "$20/month" : "$200/year"}
-          </p>
-          <Link href="/about">Learn more</Link>
-        </div>
-        <div className="h-46 bg-teal-300 dark:bg-slate-800 p-6 rounded-lg drop-shadow-xl overflow-hidden">
-          <h2 className="text-2xl text-white font-semibold p-5">Laundry</h2>
-          <p className="text-3xl text-white font-semibold mt-4">
-            {isMonthly ? "$30/month" : "$300/year"}
-          </p>
-          <Link href="/about">Learn more</Link>
-        </div>
+      {/* Frosted Glass Effect */}
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-20 backdrop-blur-md rounded-3xl px-6 py-2 whitespace-nowrap">
+        <p className="text-white text-base text-center font-semibold">
+          A powerful housekeeping experience
+        </p>
       </div>
+    </div>
+  </section>
 
-      <Image
-        src="/cleanings.png"
-        alt="all rooms covered"
-        width={1500}
-        height={100}
-      />
+            {/* Responsive Standard Subscription Card */}
+            <section className="flex overflow-hidden flex-col items-center bg-white aspect-square">
+              <div className="flex relative flex-col px-3.5 pt-7 pb-20 w-full aspect-[1.02]">
+                <Image
+                  src="/standard/Standard-card-image.png"
+                  height={100}
+                  width={1000}
+                  alt="Vip Subscription card"
+                  className="object-cover absolute inset-0 size-full"
+                />
 
-      <h1 className="flex my-20 px-10 text-4xl justify-center">
-        Order a turndown service and get a free desert on us
-      </h1>
-      <div className="flex justify-center">
-        <Image
-          src="/cookie.png"
-          alt="all rooms covered"
-          width={700}
-          height={100}
-        />
-      </div>
+                <div className="absolute top-16 left-1/2 transform -translate-x-1/2 flex items-center space-x-4">
+                  <StandardBadge price="$800" />
+                  <a href="/standardSub" className="bg-blue-600 text-white px-4 py-2 rounded-full text-sm font-semibold shadow-md hover:bg-blue-700 transition whitespace-nowrap inline-flex items-center">
+  View Details
+</a>
+                </div>
 
-      <Footer />
+                {/* Frosted Glass Effect */}
+                <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-20 backdrop-blur-md rounded-3xl px-6 py-4 whitespace-nowrap">
+                  <p className="text-white text-base text-center font-semibold">
+                    The gold standard of  housekeeping
+                  </p>
+                </div>
+              </div>
+            </section>
+
+            {/* Responsive Basic Subscription Card */}
+            <section className="flex overflow-hidden flex-col items-center bg-white aspect-square">
+              <div className="flex relative flex-col px-3.5 pt-7 pb-20 w-full aspect-[1.02]">
+                <Image
+                  src="/basic/Basic-card-image.png"
+                  height={100}
+                  width={1000}
+                  alt="Basic Subscription card"
+                  className="object-cover absolute inset-0 size-full"
+                />
+
+                <div className="absolute top-16 left-1/2 transform -translate-x-1/2 flex items-center space-x-4">
+                  <BasicBadge price="$500" />
+                  <a href="/basicSub" className="bg-blue-600 text-white px-4 py-2 rounded-full text-sm font-semibold shadow-md hover:bg-blue-700 transition whitespace-nowrap inline-flex items-center">
+  View Details
+</a>
+                </div>
+
+                {/* Frosted Glass Effect */}
+                <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-20 backdrop-blur-md rounded-3xl px-6 py-4 whitespace-nowrap">
+                  <p className="text-white text-base text-center font-semibold">
+                    Experience Cleanbot in it`s simplest form
+                  </p>
+                </div>
+              </div>
+            </section>
+
+
+          </section>
+
+
+
+
+        </div>
+      </main>
     </>
+  );
+}
+
+// Reusable Card Component
+function CardO({ title, description }) {
+  return (
+    <div className="p-6 bg-white shadow-lg rounded-lg">
+      <h2 className="text-xl font-semibold">{title}</h2>
+      <p className="mt-2 text-gray-600 px-4">{description}</p>
+    </div>
   );
 }
 
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,81 +1,85 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-function NavBar()
-{
-    const [navbar, setNavbar] = useState(false);
+function NavBar() {
+  const [navbar, setNavbar] = useState(false);
 
-    return(
-        <nav className="w-full bg-white fixed top-5 rounded-full left-0 right-0 z-10">
-            <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-                <div>
-                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                        {/* LOGO */}
-                        <Link href="/">
-                            <Image src="/dalle.png" width={40} height={40} alt="logo" />
-                        </Link>
-                        {/* HAMBURGER BUTTON */}
-                        <div className="md:hidden">
-                            <button
-                            className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                            onClick={() => setNavbar(!navbar)}
-                            >
-                            {navbar ? (
-                                <Image src="/close.png" width={30} height={30} alt="close icon" />
-                            ) : (
-                                <Image
-                                src="/hamburger_icon.png"
-                                width={30}
-                                height={30}
-                                className="focus:border-none active:border-none"
-                                alt="hamburger icon"
-                                />
-                            )}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
-                        <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                            <li className="pb-3 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-blue-300  border-black  md:hover:text-blue-300 md:hover:bg-transparent">
-                            <Link href="/subscription" onClick={() => setNavbar(!navbar)}>
-                                Subscription
-                            </Link>
-                            </li>
-                            <li className="pb-3 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-blue-300  border-black  md:hover:text-blue-300 md:hover:bg-transparent">
-                            <Link href="/cleaners" onClick={() => setNavbar(!navbar)}>
-                                Cleaners
-                            </Link>
-                            </li>
-                            <li className="pb-3 text-xl text-black py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-blue-300  border-black  md:hover:text-blue-300 md:hover:bg-transparent">
-                            <Link href="/about" onClick={() => setNavbar(!navbar)}>
-                                About
-                            </Link>
-                            </li>
-                            <li className="pb-3 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-blue-300  border-black  md:hover:text-blue-300 md:hover:bg-transparent">
-                            <Link href="/robot" onClick={() => setNavbar(!navbar)}>
-                                Robot
-                            </Link>
-                            </li>
-                            <li className="pb-3 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-blue-300  border-black  md:hover:text-blue-300 md:hover:bg-transparent">
-                            <Link href="/investors" onClick={() => setNavbar(!navbar)}>
-                                Investors
-                            </Link>
-                            </li>
-                            <li className="pb-3 text-xl text-black py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-blue-300  border-black  md:hover:text-blue-300 md:hover:bg-transparent">
-                            <Link href="/blogs" onClick={() => setNavbar(!navbar)}>
-                                Blog
-                            </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+  return (
+    <nav className="w-full bg-white fixed top-5 rounded-full left-0 right-0 z-10">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            {/* LOGO & NAME */}
+            <Link href="/" className="flex items-center space-x-2">
+              <Image src="/dalle.png" width={40} height={40} alt="logo" />
+              <span className="text-2xl font-semibold text-black">Cleanbot</span>
+            </Link>
+            {/* HAMBURGER BUTTON */}
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <Image src="/close.png" width={30} height={30} alt="close icon" />
+                ) : (
+                  <Image
+                    src="/hamburger_icon.png"
+                    width={30}
+                    height={30}
+                    className="focus:border-none active:border-none"
+                    alt="hamburger icon"
+                  />
+                )}
+              </button>
             </div>
-        </nav>
-    )
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "p-12 md:p-0 block" : "hidden"
+            }`}
+          >
+            <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+              <li className="pb-3 text-xl text-black py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-blue-300 border-black md:hover:text-blue-300 md:hover:bg-transparent">
+                <Link href="/subscription" onClick={() => setNavbar(!navbar)}>
+                  Subscription
+                </Link>
+              </li>
+              <li className="pb-3 text-xl text-black py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-blue-300 border-black md:hover:text-blue-300 md:hover:bg-transparent">
+                <Link href="/cleaners" onClick={() => setNavbar(!navbar)}>
+                  Cleaners
+                </Link>
+              </li>
+              <li className="pb-3 text-xl text-black py-2 md:px-6 text-center border-b-2 md:border-b-0 hover:bg-blue-300 border-black md:hover:text-blue-300 md:hover:bg-transparent">
+                <Link href="/about" onClick={() => setNavbar(!navbar)}>
+                  About
+                </Link>
+              </li>
+              <li className="pb-3 text-xl text-black py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-blue-300 border-black md:hover:text-blue-300 md:hover:bg-transparent">
+                <Link href="/robot" onClick={() => setNavbar(!navbar)}>
+                  Robot
+                </Link>
+              </li>
+              <li className="pb-3 text-xl text-black py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-blue-300 border-black md:hover:text-blue-300 md:hover:bg-transparent">
+                <Link href="/investors" onClick={() => setNavbar(!navbar)}>
+                  Investors
+                </Link>
+              </li>
+              <li className="pb-3 text-xl text-black py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-blue-300 border-black md:hover:text-blue-300 md:hover:bg-transparent">
+                <Link href="/blogs" onClick={() => setNavbar(!navbar)}>
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
